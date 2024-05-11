00d2d4d9f87621c5525a0e18a3d717bda442c62f
azizjamli <jamliaziz075@gmail.com>
2024-05-11 01:42:54 +0100

    dz

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const userRoutes = require('./routes/userroutes');
 
@@ -10,6 +12,9 @@ app.use(express.json());
 
 // Enable CORS for all routes
 app.use(cors());
+
+app.use(cookieParser());
+
 
 // Use the user routes
 app.use('/api/users', userRoutes);
