09303f15878f0cec0f6ff6d4ce7fb1a08d926601
azizjamli <jamliaziz075@gmail.com>
2024-05-12 04:33:07 +0100

    qdz

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,27 @@
 require('dotenv').config();
-
 const express = require('express');
-
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
-const app = express();
 const userRoutes = require('./routes/userroutes');
 const authRoutes = require('./routes/authRoutes');
 
-
+const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Enable CORS for all routes
-app.use(cors());
+// Enable CORS for all routes with specific origin and credentials
+const corsOptions = {
+  origin: 'http://localhost:3000', // Set the specific origin of your client-side application
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
-
 
 // Use the user routes
 app.use('/api/users', userRoutes);
 app.use('/api', authRoutes);
-
 
 // Start the server
 const PORT = process.env.PORT || 3001;
