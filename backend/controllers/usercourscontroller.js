d0f5bccdd11f57cf9025dbd064b443a981a3606f
azizjamli <jamliaziz075@gmail.com>
2024-05-15 01:24:37 +0100

    qdz

--- backend/controllers/usercourscontroller.js
+++ backend/controllers/usercourscontroller.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
-// Import necessary models
 const usercours = require('../models/userscours');
-const User = require('../models/users'); // Import the User model
-const Cours = require('../models/cours'); // Import the Cours model
+const User = require('../models/users');
+const Cours = require('../models/cours');
+const { Op, Sequelize } = require('sequelize');
+const sequelize = require('../dbConfig'); // Import the sequelize instance
 
-// CREATE (POST)
 const coursfinder = async (req, res) => {
   try {
-    // Extract userID and selectedCategoryId from request query parameters
     const userID = req.query.userId;
     const selectedCategoryId = req.query.selectedCategoryId;
 
@@ -17,27 +15,81 @@ const coursfinder = async (req, res) => {
       return res.status(400).json({ error: 'User ID or Selected Category ID missing in query parameters' });
     }
 
-    // Query usercours table to get courses for the given user ID and category ID
     const userCourses = await usercours.findAll({
-      attributes: [ 'createdAt', 'updatedAt', 'coursId'], // Specify the attributes to select from userCourses
+      attributes: ['createdAt', 'updatedAt', 'coursId'],
       include: [
         {
-          model: Cours, // Include the Cours model
-          attributes: ['id', 'titre', 'description', 'prix', 'photo', 'categorieId'], // Specify the attributes to select from Cours
+          model: Cours,
+          attributes: ['id', 'titre', 'description', 'prix', 'photo', 'categorieId'],
           where: { categorieId: selectedCategoryId },
-          as: 'Cour', // Correct alias to match the association
+          as: 'Cour',
         },
       ],
       where: { userId: userID },
     });
 
-    res.status(200).json(userCourses); // Return the userCourses directly since they already contain the necessary data
+    res.status(200).json(userCourses);
   } catch (error) {
     console.error('Error fetching user courses:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 };
 
+const coursfindernouser = async (req, res) => {
+  try {
+    const userId = req.query.userId;
+
+    if (!userId) {
+      return res.status(400).json({ error: 'User ID missing in query parameters' });
+    }
+
+    // Find all courses from the Cours table that the user hasn't added (not in usercours table for the user)
+    const coursesNotAdded = await Cours.findAll({
+      include: [
+        {
+          model: User,
+          through: {
+            model: usercours,
+            where: { userId: userId },
+            attributes: [], // Exclude userId from the result
+          },
+          attributes: [], // Exclude User attributes from the result
+        },
+      ],
+      where: { '$Users.id$': null }, // Check for courses not associated with any user
+    });
+
+    res.status(200).json(coursesNotAdded);
+  } catch (error) {
+    console.error('Error fetching courses not added by user:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
+const allcoursinusercours = async (req, res) => {
+  try {
+    const courses = await Cours.findAll();
+
+    if (!courses || courses.length === 0) {
+      return res.status(404).json({ error: 'No courses found' });
+    }
+
+    const promises = courses.map(async (course) => {
+      await usercours.create({ coursId: course.id });
+    });
+
+    await Promise.all(promises);
+
+    res.status(200).json({ message: 'Courses added to usercours table successfully' });
+  } catch (error) {
+    console.error('Error adding courses to usercours:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
 module.exports = {
   coursfinder,
+  coursfindernouser,
+  allcoursinusercours
 };
