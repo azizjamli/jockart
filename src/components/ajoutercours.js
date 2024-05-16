a18c0c9a2ade7ab4bb5675ea206930326e4972fc
azizjamli <jamliaziz075@gmail.com>
2024-05-16 01:47:08 +0100

    qzd

--- src/components/ajoutercours.js
+++ src/components/ajoutercours.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
 import axios from 'axios';
 
 const Ajoutercours = () => {
     const { id } = useParams(); // Get the course ID from the URL params
     const userId = localStorage.getItem('userId'); // Get the userId from localStorage
+    const navigate = useNavigate(); // Use useHistory hook to access history object
+
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post('http://localhost:3001/api/usercours/addCourseToUser', { courseId: id, userId });
             console.log(response.data); // Handle the response as needed
+            navigate('/Dashboardetud');
+
         } catch (error) {
             console.error('Error adding course to user:', error);
         }
