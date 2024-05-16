e5525ab6926c0919e3a7b405b4412da6c1e8e3f4
azizjamli <jamliaziz075@gmail.com>
2024-05-16 02:07:17 +0100

    hg

--- src/components/accedercours.js
+++ src/components/accedercours.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import UserInfo from './UserInfo';
 
-const Accedercours = () =>{
-    return(
-        <h2>iqshd</h2>
-    );
+const Accedercours = () => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const userId = localStorage.getItem('userId');
+        const userResponse = await axios.get('http://localhost:3001/api/users/getInfo', {
+          params: { userId },
+        });
+        setUser(userResponse.data);
+        console.log(userResponse.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching user info:', error);
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <h2>iqshd</h2>
+      <UserInfo user={user} />
+
+      <div className='container bg-success'>
+        <h2>cours</h2>
+
+
+      </div>
+    </>
+  );
 };
+
 export default Accedercours;
