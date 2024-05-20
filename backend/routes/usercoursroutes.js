8cdbf649c1dea347c96c483dda9d733f71b5a4be
azizjamli <jamliaziz075@gmail.com>
2024-05-20 23:52:04 +0100

    qzd

--- backend/routes/usercoursroutes.js
+++ backend/routes/usercoursroutes.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { coursfinder , coursfindernouser , allcoursinusercours , addCourseToUser} = require('../controllers/usercourscontroller');
-//const cookieJwtAuth = require('../middleware/cookieJwtAuth');
-
+const { coursfinder, coursfindernouser, allcoursinusercours, addCourseToUser, getCoursUsers } = require('../controllers/usercourscontroller');
 
 router.get('/coursfinder', coursfinder);
 router.get('/coursfindernouser', coursfindernouser);
 router.get('/allcoursinusercours', allcoursinusercours);
-router.post('/addCourseToUser' , addCourseToUser);
+router.post('/addCourseToUser', addCourseToUser);
 
-
-
-
-// GET /api/user/profile - User profile route with middleware
+// New route for fetching users associated with a course
+router.get('/getCoursUsers/:courseId', getCoursUsers);
 
 module.exports = router;
- 
