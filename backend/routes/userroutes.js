3bf61359b635fbf2ff20578394ca5e592db49ba5
azizjamli <jamliaziz075@gmail.com>
2024-05-26 16:06:11 +0100

    update user photo

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -1,36 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { signin, signup, getInfo, uploadPhoto } = require('../controllers/usercontroller');
+const { signin, signup, getInfo, uploadPhoto, updateUser } = require('../controllers/usercontroller');
 const multer = require('multer');
 const path = require('path');
 
 // Set up storage engine for multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../uploads/user')); // Save files to uploads/user folder
+    cb(null, path.join(__dirname, '../uploads/user')); // Save files to uploads/user
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname)); // Use timestamp to avoid filename conflicts
-  },
+  }
 });
 
-// Initialize upload variable
-const upload = multer({ 
-  storage: storage,
-  limits: { fileSize: 1024 * 1024 * 5 }, // Limit file size to 5MB
-  fileFilter: function (req, file, cb) {
-    // Check file type
-    const filetypes = /jpeg|jpg|png/;
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
-
-    if (extname && mimetype) {
-      return cb(null, true);
-    } else {
-      cb('Error: Only JPEG, JPG, or PNG files are allowed!');
-    }
-  },
-});
+const upload = multer({ storage: storage });
 
 // POST /api/users/signin - User sign-in route
 router.post('/signin', signin);
@@ -43,5 +27,8 @@ router.get('/getInfo', getInfo);
 
 // POST /api/users/uploadPhoto/:userId - Upload user photo route
 router.post('/uploadPhoto/:userId', upload.single('photo'), uploadPhoto);
+
+// PUT /api/users/updateUser/:userId - Update user details including photo
+router.put('/updateUser/:userId', upload.single('photo'), updateUser);
 
 module.exports = router;
